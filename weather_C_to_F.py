ebddc3b59f5326f5b5f40a309105f030befa4ba9
gloriaradicheva <114574688+gloriaradicheva@users.noreply.github.com>
2024-03-11 10:31:18 +0200

    Add files via upload

---
+++ weather_C_to_F.py
@@ -0,0 +1,12 @@
+def convert_celsius_to_fahrenheit(celsius):
+
+  return (celsius * 1.8) + 32
+
+
+def weather_script(script_name):
+
+  return script_content
+
+script_content = weather_script("weather2.sh")
+
+print(script_content)
