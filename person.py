6fef64223e8a2c11ece68eb1802096f239474ed3
gloriaradicheva <114574688+gloriaradicheva@users.noreply.github.com>
2024-03-11 10:29:42 +0200

    Add files via upload

---
+++ person.py
@@ -0,0 +1,9 @@
+def person(name, surname, age, location):
+    print(f"Hello, my name is {name} {surname}, I am {age} years old and I am from {location}")
+
+
+name = "Gloria"
+surname = "Radicheva"
+age = 27
+location = "Sofia"
+print(person(name,surname,age,location))
